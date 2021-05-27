e7aca41cba233b8c676f002df54f58608e79adbb
urbpdx <scott.urbatsch@greatvines.com>
2021-05-27 08:33:12 -0700

    Fixed Width for Parameter Value

--- Global/dbo/Tables/ClientParameter.sql
+++ Global/dbo/Tables/ClientParameter.sql
@@ -2,7 +2,7 @@
     [ClientID]         INT           NOT NULL,
     [ParameterID]      INT           NOT NULL,
     [EnvironmentID]    INT           NULL,
-    [ParameterValue]   VARCHAR (100) NULL,
+    [ParameterValue]   VARCHAR (255) NULL,
     [CreateDate]       DATETIME      CONSTRAINT [DF_ClientParameter_CreateDate] DEFAULT (getdate()) NOT NULL,
     [LastModifiedDate] DATETIME      CONSTRAINT [DF_ClientParameter_LastModifiedDate] DEFAULT (getdate()) NOT NULL, 
     [Active] BIT NOT NULL DEFAULT 1
