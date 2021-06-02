6115dc150f2660efd348939fd0e5eb9a731ecaa2
urbpdx <scott.urbatsch@greatvines.com>
2021-06-02 12:34:53 -0700

    Cleaning up some of the tables

--- LiquorWarehouseStage_1_0/SRSIn/Tables/CTLDA.sql
+++ LiquorWarehouseStage_1_0/SRSIn/Tables/CTLDA.sql
@@ -3,7 +3,7 @@
 	[DISTID] [varchar](8) NOT NULL,
 	[VIPSRS] [varchar](5) NOT NULL,
 	[DISTNM] [varchar](40) NOT NULL,
-	[CTITEM] [varchar](10) NOT NULL,
+	[CTITEM] [varchar](30) NOT NULL,
 	[CTQTY] [numeric](10, 0) NOT NULL,
 	[CTUOM] [varchar](1) NOT NULL,
 	[CTDATE] [varchar](6) NOT NULL,
