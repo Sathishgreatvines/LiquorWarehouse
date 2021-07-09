0ac2be73ede88803532bb6ebfbba6236a94d92a1
urbpdx <scott.urbatsch@greatvines.com>
2021-07-08 21:13:02 -0700

    https://greatvines.atlassian.net/browse/LW-1189

--- LiquorWarehouseStage_1_0/SRSXREF/Tables/AccountXRef.sql
+++ LiquorWarehouseStage_1_0/SRSXREF/Tables/AccountXRef.sql
@@ -1,12 +1,12 @@
 ﻿CREATE TABLE [SRSXREF].[AccountXRef](
 	[Id] [char](18) NOT NULL,
-	[Name] [nvarchar](250) NULL,
-	[BillingStreet] [nvarchar](255) NULL,
-	[ROADDR1] [varchar](35) NOT NULL,
-	[RODBA] [varchar](35) NOT NULL,
-	[gvp__Dist_Acct_Number__c] [varchar](40) NULL,
-	[gvp__Account_Key_List__c] [varchar](4000) NULL,
-	[DISTID] [varchar](25) NOT NULL,
+	[SFName] [nvarchar](250) NULL,
+	[SFBillingStreet] [nvarchar](255) NULL,
+	[OUTDAROADDR1] [varchar](35) NOT NULL,
+	[OUTDARODBA] [varchar](35) NOT NULL,
+	[SFgvp__Dist_Acct_Number__c] [varchar](40) NULL,
+	[SFgvp__Account_Key_List__c] [varchar](4000) NULL,
+	[OUTDADISTID] [varchar](25) NOT NULL,
 	[VPST] [varchar](2) NOT NULL
 ) ON [PRIMARY]
 GO
