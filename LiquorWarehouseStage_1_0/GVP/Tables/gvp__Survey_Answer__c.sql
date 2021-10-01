55300f1f7a68541722dd1fafca1c907732f901ac
urbpdx <scott.urbatsch@greatvines.com>
2021-10-01 11:48:50 -0700

    Mostly BevPath updates

--- LiquorWarehouseStage_1_0/GVP/Tables/gvp__Survey_Answer__c.sql
+++ LiquorWarehouseStage_1_0/GVP/Tables/gvp__Survey_Answer__c.sql
@@ -60,7 +60,11 @@
 
 GO
 
-CREATE INDEX IX_SurveyAnswer_Survey ON [GVP].[gvp__Survey_Answer__c] ([gvp__Survey__c]) INCLUDE ([Name], [CreatedDate], [SystemModstamp], [gvp__Answer_Check__c], [gvp__Answer_Currency__c], [gvp__Answer_Number__c], [gvp__Answer_Percent__c], [gvp__Answer_Text__c], [gvp__Order__c], [gvp__Question__c], [gvp__Points__c], [gvp__Most_Recent__c], [gvp__Matching_Target_Points__c], [gvp__External_Id__c], [gvp__Answer_Formula__c], [gvp__Custom_Currency_1__c], [gvp__Custom_Fact_1__c], [gvp__Custom_Fact_2__c], [gvp__Custom_Fact_3__c], [gvp__Custom_Fact_4__c], [gvp__Custom_Fact_5__c], [gvp__Custom_Text_1__c], [gvp__Custom_Text_2__c], [gvp__Custom_Text_3__c], [gvp__Custom_Text_4__c], [gvp__Custom_Text_5__c], [gvp__Audited_Survey_Answer__c], [gvp__Attachments__c], [gvp__Answer_Brand__c], [gvp__Answer_Competitor__c], [gvp__Answer_Item__c], [gvp__Answer_Label__c], [gvp__Answer_Product_Set__c], [gvp__Answer_Size__c])
+CREATE INDEX IX_SurveyAnswer_Survey ON [GVP].[gvp__Survey_Answer__c]
+  ([gvp__Survey__c]) INCLUDE ([Name], [CreatedDate], [SystemModstamp], [gvp__Answer_Check__c], [gvp__Answer_Currency__c], [gvp__Answer_Number__c], [gvp__Answer_Percent__c], [gvp__Answer_Text__c], [gvp__Order__c], [gvp__Question__c], [gvp__Points__c], [gvp__Most_Recent__c], [gvp__Matching_Target_Points__c], [gvp__External_Id__c], [gvp__Answer_Formula__c], [gvp__Custom_Currency_1__c], [gvp__Custom_Fact_1__c], [gvp__Custom_Fact_2__c], [gvp__Custom_Fact_3__c], [gvp__Custom_Fact_4__c], [gvp__Custom_Fact_5__c], [gvp__Custom_Text_1__c], [gvp__Custom_Text_2__c], [gvp__Custom_Text_3__c], [gvp__Custom_Text_4__c], [gvp__Custom_Text_5__c], [gvp__Audited_Survey_Answer__c], [gvp__Attachments__c], [gvp__Answer_Brand__c], [gvp__Answer_Competitor__c], [gvp__Answer_Item__c], [gvp__Answer_Label__c], [gvp__Answer_Product_Set__c], [gvp__Answer_Size__c])
+go
+CREATE NONCLUSTERED INDEX [IX_SurveyAnswer_ID] ON [GVP].[gvp__Survey_Answer__c]
+  ([Id])
 go
 CREATE NONCLUSTERED INDEX [IX_SurveyAnswer_ID_Survey] ON [GVP].[gvp__Survey_Answer__c]
 (
---
+++ LiquorWarehouseStage_1_0/SFOut/Stored Procedures/AcctXRefMergeSharedAccts.sql
@@ -0,0 +1,58 @@
+﻿CREATE PROCEDURE [SFOut].[AcctXRefMergeSharedAccts]
+	@SrcSchema varchar(20) = 'BevPathIn',
+	@ConnectionId varchar(255) = 'gvp__BevPath_Connection_ID__c'
+AS
+	IF OBJECT_ID('tempdb..#TempXREF') IS NOT NULL DROP TABLE #TempXREF;
+
+Select 
+ distinct RSCUST, RawState, AccountName, AccountKey, SFID  
+into #TempXREF
+from SRSXREF.AccountXRef ax
+where ax.RawState in (select distinct gvp__Account_Address_State__c from BevPathIn.gvp__Shared_Account__c);
+
+WITH groups AS (
+ SELECT distinct
+       coalesce(an.SFID, ax.SFID, ak.SFID, ay.SFID) as gvp__Account__c
+	  ,bp.id as [gvp__BevPath_Connection__c]
+      ,gsa.gvp__Received_Account_Identifier__c
+      ,gsa.[gvp__External_Id__c]
+      ,[gvp__Account_Key__c]
+      ,[gvp__Account_Key_List__c]
+      ,coalesce(an.AccountName,ax.AccountName,ak.AccountName,ay.AccountName,[gvp__Account_Name__c]) as gvp__Account_Name__c
+      ,[gvp__Account_Segment__c]
+      ,[gvp__Classification__c]
+      ,[gvp__Attribute_1__c]
+      ,[gvp__Attribute_2__c]
+      ,[gvp__Attribute_3__c]
+      ,[gvp__Attribute_4__c]
+      ,[gvp__Attribute_5__c]
+      ,[gvp__Attribute_6__c]
+      ,[gvp__Attribute_7__c]
+      ,[gvp__Attribute_8__c]
+      ,[gvp__Attribute_9__c]
+      ,[gvp__Attribute_10__c]
+      ,gvp__Account_Full_Address__c
+      ,gvp__Account_Address_Street__c
+      ,gvp__Account_Address_City__c
+      ,gvp__Account_Address_State__c
+      ,gvp__Account_Address_Zip_Code__c
+      ,gvp__County__c
+      ,[gvp__DBA__c]
+      ,[gvp__Distributor_Account_Number__c]
+      ,[gvp__Distributor_Rep__c]
+      ,[gvp__Labels__c]
+      ,gsa.[gvp__Account_MSA_Code__c]
+      ,gsa.[gvp__Account_MSA_Name__c]
+      ,gsa.gvp__PremiseType__c
+      ,[gvp__Region__c]
+      ,[gvp__Territory__c]
+	   ,ROW_NUMBER() OVER (Partition by gsa.gvp__External_Id__c order by gsa.gvp__Account_Status__c asc) as RowNumber
+  FROM QUOTENAME([@SrcSchema]).[gvp__Shared_Account__c] gsa
+  left join #TempXREF an on (an.RSCUST = gsa.gvp__Received_Account_Identifier__c and an.RawState = gsa.gvp__Account_Address_State__c and an.AccountName = gsa.gvp__Account_Name__c)
+  left join #TempXREF ax on (ax.RSCUST = gsa.gvp__Received_Account_Identifier__c and ax.RawState = gsa.gvp__Account_Address_State__c)
+  left join #TempXREF ay on (ay.AccountKey = gsa.gvp__Received_Account_Identifier__c and ay.RawState = gsa.gvp__Account_Address_State__c)
+  left join #TempXREF ak on (ak.RSCUST = gsa.gvp__Received_Account_Identifier__c)
+  left join gvp.gvp__BevPath_Connection__c bp on bp.[@ConnectionId] = gsa.gvp__BevPath_Connection__c 
+)
+select * from groups
+  where ROWNUMBER = 1
