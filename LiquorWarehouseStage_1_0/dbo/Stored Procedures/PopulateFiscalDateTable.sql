9bb49074844d24a2941a68d7a874ddc8221ccb98
Heidi Salava <heidi.salava@greatvines.com>
2021-10-14 06:02:16 -0700

    Fix dates

--- LiquorWarehouseStage_1_0/dbo/Stored Procedures/PopulateFiscalDateTable.sql
+++ LiquorWarehouseStage_1_0/dbo/Stored Procedures/PopulateFiscalDateTable.sql
@@ -1,14 +1,18 @@
 ﻿
 GO
 
-/****** Object:  StoredProcedure [dbo].[PopulateFiscalDateTable]    Script Date: 10/13/2021 2:54:57 PM ******/
+/****** Object:  StoredProcedure [dbo].[PopulateFiscalDateTable]    Script Date: 10/14/2021 5:39:21 AM ******/
 SET ANSI_NULLS OFF
 GO
 
 SET QUOTED_IDENTIFIER OFF
 GO
 
-CREATE PROCEDURE [dbo].[PopulateFiscalDateTable] @fiscalmonthadjust int, @start varchar(10), @end varchar(10), @country char(3)
+
+USE [aaHeidi_LiquorWarehouseStage_1_03_05]
+go
+
+CREATE OR ALTER       PROCEDURE [dbo].[PopulateFiscalDateTable] @fiscalmonthadjust int, @start varchar(10), @end varchar(10), @country char(3)
 as
 begin
   -- Convert parameters to dates
@@ -17,42 +21,64 @@ begin
   set @startdate = convert(date, @start)
   set @enddate = convert(date, @end)
 
+
   -- Get the days offset
-  declare @dayoffset int, @weekoffset int
-  set @dayoffset = datediff(DD, convert(date, '1/1/' + convert(varchar, year(@fiscalyearstartdate)+1)), @fiscalyearstartdate)
+  declare @dayoffset int, @weekoffset int, @dayoffsetEnd int
+  set @dayoffset = datediff(DD, convert(date, '1/1/' + convert(varchar, year(@fiscalyearstartdate))), @fiscalyearstartdate)
+  set @dayoffsetend = datediff(DD, @fiscalyearstartdate, convert(date, '12/31/' + convert(varchar, year(@fiscalyearstartdate)))) + 1
+
 
   -- Delete dates that already exist in the FiscalDate table
   delete from FiscalDate where FiscalDate between @startdate and @enddate
 
-  insert into FiscalDate (FiscalDate, ReportDate, DayNumberOfYear, DayNumberOfQuarter, DayNumberOfMonth, DayNumberOfWeek, WeekdaySN, WeekdayLN, WeekNumberOfYear, MonthSN, MonthLN, MonthNumberOfYear, MonthNumberOfQuarter, QuarterNumberOfYear,
+  insert into FiscalDate (FiscalDate, ReportDate, DayNumberOfYear, DayNumberOfQuarter, DayNumberOfMonth, DayNumberOfWeek, WeekdaySN, WeekdayLN, WeekNumberOfYear,
+  MonthSN, MonthLN, MonthNumberOfYear, MonthNumberOfQuarter, QuarterNumberOfYear,
     [Year], CalendarYearMonth, [WeekDay], Holiday, HolidayName, HolidayNameEnglish, DateNumber, FiscalDateNumber)
     select 
-    	CD.CalendarDate as FiscalDate,
-		FD.CalendarDate as ReportDate,
-      FD.DayNumberOfYear as DayNumberOfYear,
-	    FD.DayNumberOfQuarter as DayNumberOfQuarter,
-	    CD.DayNumberOfMonth,
-      CD.DayNumberOfWeek,
-      CD.WeekdaySN, 
-      CD.WeekdayLN,
-      FD.WeekNumberOfYear as WeekNumberOfYear,
-	  format (DATEADD(MONTH,-1 * @fiscalmonthadjust,fd.CalendarDate), 'MMM', 'en-US') as MonthSN,
-	  datename (month, (DATEADD(MONTH,-1 * @fiscalmonthadjust,fd.CalendarDate))) as MonthLN,
-      FD.MonthNumberOfYear as MonthNumberOfYear,
-      FD.MonthNumberOfQuarter MonthNumberOfQuarter, 
-      FD.QuarterNumberOfYear,
-      FD.[Year],
-      CD.YearMonth,
-	    CD.WeekDay,
+		CD.CalendarDate as FiscalDate, 
+		FD.CalendarDate as ReportDate, 
+		case when cd.daynumberofyear > @dayoffset  then cd.DayNumberOfYear -  @dayoffset
+		     else cd.daynumberofyear + @dayoffsetend
+			 End 			 as DayNumberOFYear,
+		
+		FD.DayNumberOfQuarter as DayNumberOfQuarter, 
+	    CD.DayNumberOfMonth, 
+		CD.DayNumberOfWeek, 
+		CD.WeekdaySN, 
+		CD.WeekdayLN, 
+		FD.WeekNumberOfYear - FD.WeekNumberOfYear + CD.WeekNumberOfYear as WeekNumberOfYear,
+		cast(SUBSTRING(DATENAME(month, CD.CALENDARDATE), 1, 3) as char(3))  AS MonthSN ,
+				DATENAME(month, CD.CALENDARDATE) AS MonthLN, 
+			
+		FOM.FiscalValueYear as MonthNumberOfYear,
+		FOM.FiscalValueQuarter as  MonthNumberOfQuarter, 
+		FOQ.FiscalValueYear as QuarterNumberOfYear,
+		Case when  Cast(datepart(month, CD.CalendarDate) as int)  >  @fiscalmonthadjust
+		      then CD.[YEAR]
+			  ELSE CD.YEAR -1
+			
+			END, 
+	  	   
+		CD.YearMonth,
+		CD.WeekDay,
       case when h.HolidayName is null then 0 else 1 end,
       h.HolidayName,
       h.HolidayNameEnglish,
       CD.DateNumber,
-	  FD.DateNumber as FiscalDateNumber
-
+	  FD.DateNumber as FiscalDateNumber 
     from Global..CalendarDate CD
 		  inner join Global..CalendarDate FD on dateadd(dd, datediff(DD,@startdate, dateadd(MM, @fiscalmonthadjust, @start)),CD.CalendarDate) = FD.CalendarDate 
+		  
+		  inner join[dbo].[FiscalOffsets]  FOM on CD.MonthNumberOfYEAR = FOM.[Month] 
+								and FOM.OffsetType = 'MONTHYEAR'
+								and FOM.Offset = @fiscalmonthadjust
 
+		  inner Join [dbo].[FiscalOffsets]  FOQ on  CD.MonthNumberOfYEAR = FOQ.[Month]
+		         and FOQ.OffsetType = 'QUARTER'
+		         and FOQ.Offset = @fiscalMonthAdjust
+
+				
+				 
       left outer join Global..Holiday h on CD.CalendarDate = h.CalendarDate and h.Country = @country
     where CD.CalendarDate between @startdate and @enddate
     order by FiscalDate
