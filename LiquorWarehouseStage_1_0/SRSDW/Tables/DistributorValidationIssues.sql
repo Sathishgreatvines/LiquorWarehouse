4a8b58164b822b75c74bd0d7da8a39fead4c8395
FacileAsheesh <asheesh@facileconsulting.com>
2021-10-25 20:32:50 +0530

    Prduct validation, and Issues Rename Issues Fixed Distribution validation Order Issues Fixed

--- LiquorWarehouseStage_1_0/SRSDW/Tables/DistributorValidationIssues.sql
+++ LiquorWarehouseStage_1_0/SRSDW/Tables/DistributorValidationIssues.sql
@@ -1,10 +1,10 @@
 ﻿CREATE TABLE [SRSDW].[DistributorValidationIssues](
 	[SRSDistID] [varchar](10) NULL,
 	[SRSAltDistID] [varchar](25) NULL,
-	[GVPAccountKey] [varchar](70) NULL,
 	[SRSName] [nvarchar](255) NULL,
+	[GVPAccountKey] [varchar](70) NULL,
 	[GVPName] [nvarchar](255) NULL,
 	[CTLSSum] [varchar](30) NULL,
 	[ErrorType] [nvarchar](255) NULL,
 	[ErrorMessage] [nvarchar](500) NULL
-) 
+)
